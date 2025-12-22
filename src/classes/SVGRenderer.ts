c0676c7d60c024bd96e866fe31aaddf902eb97f3
DrakeB1234 <dbuentello211607@gmail.com>
2025-12-22 15:29:11 -0600

    Removed unsued declarations.

--- src/classes/SVGRenderer.ts
+++ src/classes/SVGRenderer.ts
@@ -1,4 +1,4 @@
-import { NAMESPACE, NOTE_LAYER_START_X } from "../constants";
+import { NAMESPACE } from "../constants";
 import { GLPYH_ENTRIES, type GlyphNames } from "../glyphs";
 
 export const SVG_HREF = "http://www.w3.org/2000/svg";
